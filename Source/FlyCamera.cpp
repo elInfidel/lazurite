365de21c6ed9eb3e6607b949fbe1f1f095e60254
Liam Parker - s3372913 <s3372913@student.rmit.edu.au>
2016-08-01 13:50:33 +1000

    Transform optimizations & Camera rotation work

--- Source/FlyCamera.cpp
+++ Source/FlyCamera.cpp
@@ -9,7 +9,7 @@ FlyCamera::~FlyCamera() {}
 void FlyCamera::Update(float deltaTime)
 {
 	CalculateMovement(deltaTime);
-	//CalculateRotation(deltaTime);
+	CalculateRotation(deltaTime);
 }
 
 void FlyCamera::CalculateRotation(float deltaTime)
@@ -19,21 +19,17 @@ void FlyCamera::CalculateRotation(float deltaTime)
 
 	curOffset = Input::GetInstance()->GetMouseDelta();
 
-	if (curOffset == lastOffset)
-	{
-		yaw += 0.0f;
-		pitch += 0.0f;
-	}
-	else
+	if (curOffset != lastOffset)
 	{
 		yaw += curOffset.x * sensitivity;
 		pitch += curOffset.y * sensitivity;
 	}
+	else
+	{
+		yaw = pitch = 0.0f;
+	}
 
-	if (pitch > 89.0f)
-		pitch = 89.0f;
-	if (pitch < -89.0f)
-		pitch = -89.0f;
+	glm::clamp(pitch, -89.0f, 89.0f);
 
 	// We also recalculate our direction vectors // TODO: Move this to transform class?
 	transform.SetForward(vec3(cos(glm::radians(pitch)) * cos(glm::radians(yaw)), sin(glm::radians(pitch)), cos(glm::radians(pitch)) * sin(glm::radians(yaw))));
