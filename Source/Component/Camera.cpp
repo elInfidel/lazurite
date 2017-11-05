b15121e8027ab6586e60358982a1bef1ac871f5b
Liam Parker <parker.liam5@gmail.com>
2017-11-05 20:27:12 +1100

    Camera Cleanup
    Variable renaming & removal of unused functions

--- Source/Component/Camera.cpp
+++ Source/Component/Camera.cpp
@@ -5,30 +5,30 @@
 
 Camera::Camera()
 {
-	view = mat4(1.0f);
-	projection = mat4(1.0f);
+	_view = mat4(1.0f);
+	_projection = mat4(1.0f);
 }
 
 void Camera::SetOrthographic(float left, float right, float bottom, float top, float zNear = 1.0f, float zFar = 100.0f)
 {
-	projection = glm::ortho(left, right, bottom, top, zNear, zFar);
+	_projection = glm::ortho(left, right, bottom, top, zNear, zFar);
 }
 
 void Camera::SetPerspective(float fov, float aspect, float zNear = 1.0f, float zFar = 100.0f)
 {
-	projection = glm::perspective(fov, aspect, zNear, zFar);
+	_projection = glm::perspective(fov, aspect, zNear, zFar);
 }
 
 const mat4& Camera::GetViewMatrix()
 {
 	auto transform = gameObject.get()->GetComponent<Transform>().lock().get();
-	view = glm::inverse(transform->GetWorldMatrix());
-	return view;
+	_view = glm::inverse(transform->GetWorldMatrix());
+	return _view;
 }
 
 const mat4& Camera::GetProjectionMatrix() const
 {
-	return projection;
+	return _projection;
 }
 
 void Camera::Tick(float deltaTime)
--- Source/Component/Camera.h
+++ Source/Component/Camera.h
@@ -8,20 +8,17 @@ using glm::vec3;
 
 class Camera : public AComponent
 {
+private:
+	// Camera view projection data
+	mat4 _view;
+	mat4 _projection;
+
 public:
 	Camera();
 
-	// Movement related controls
-	// These will be moved to either scripting or a sub class later.
-	const float movementSpeed = 10.0f;
-	const float sensitivity = 5.0f;
-	float yaw, pitch;
-	void CalculateRotation(float deltaTime);
-	void CalculateMovement(float deltaTime);
-
-	// Sets the camera to orthographic with the given arguments
+	// Sets the camera to orthographic projection with the given arguments
 	void SetOrthographic(float left, float right, float top, float bottom, float zNear, float zFar);
-	// Sets the camera to perspective with the given arguments
+	// Sets the camera to perspective projection with the given arguments
 	void SetPerspective(float fov, float aspect, float zNear, float zFar);
 
 	const mat4& GetViewMatrix();
@@ -29,9 +26,4 @@ public:
 
 	// Called once per update passing accessing to current deltaTime
 	virtual void Tick(float deltaTime);
-
-private:
-	// Camera view projection data
-	mat4 view;
-	mat4 projection;
 };
