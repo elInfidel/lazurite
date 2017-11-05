24b0773452f3470fc31967b5e4f083db5a1eff0a
Liam Parker <parker.liam5@gmail.com>
2017-11-05 20:01:56 +1100

    Camera updates
    Removed unused code & solved issue with returning temp variable.

--- Source/Component/Camera.cpp
+++ Source/Component/Camera.cpp
@@ -22,7 +22,8 @@ void Camera::SetPerspective(float fov, float aspect, float zNear = 1.0f, float z
 const mat4& Camera::GetViewMatrix()
 {
 	auto transform = gameObject.get()->GetComponent<Transform>().lock().get();
-	return glm::inverse(transform->GetWorldMatrix());
+	view = glm::inverse(transform->GetWorldMatrix());
+	return view;
 }
 
 const mat4& Camera::GetProjectionMatrix() const
@@ -32,54 +33,5 @@ const mat4& Camera::GetProjectionMatrix() const
 
 void Camera::Tick(float deltaTime)
 {
-	CalculateMovement(deltaTime);
-	CalculateRotation(deltaTime);
-}
 
-void Camera::CalculateRotation(float deltaTime)
-{
-	static vec2 lastOffset;
-	static vec2 curOffset;
-	auto transform = gameObject.get()->GetComponent<Transform>().lock().get();
-
-	curOffset = Input::GetInstance()->GetMouseDelta();
-
-	if (curOffset != lastOffset)
-	{
-		yaw += curOffset.x * sensitivity;
-		pitch += curOffset.y * sensitivity;
-	}
-	else
-	{
-		yaw = pitch = 0.0f;
-	}
-
-	glm::clamp(pitch, -89.0f, 89.0f);
-
-	// We also recalculate our direction vectors // TODO: Move this to transform class?
-	transform->SetForward(vec3(cos(glm::radians(pitch)) * cos(glm::radians(yaw)), sin(glm::radians(pitch)), cos(glm::radians(pitch)) * sin(glm::radians(yaw))));
-	lastOffset = curOffset;
-}
-
-void Camera::CalculateMovement(float deltaTime)
-{
-	auto transform = gameObject.get()->GetComponent<Transform>().lock().get();
-	glm::vec3 right = transform->GetRight();
-	glm::vec3 up = transform->GetUp();
-
-	// Translation
-	// TODO: Wrap key enumeration in Input class instead of using GLFW directly
-	float finalSpeed = deltaTime;
-	if (Input::GetInstance()->GetKey(GLFW_KEY_W))
-		transform->Translate(vec3(0, 0, -1) * finalSpeed);
-	if (Input::GetInstance()->GetKey(GLFW_KEY_S))
-		transform->Translate(vec3(0, 0, 1) * finalSpeed);
-	if (Input::GetInstance()->GetKey(GLFW_KEY_A))
-		transform->Translate(-right * finalSpeed);
-	if (Input::GetInstance()->GetKey(GLFW_KEY_D))
-		transform->Translate(right * finalSpeed);
-	if (Input::GetInstance()->GetKey(GLFW_KEY_SPACE))
-		transform->Translate(up * finalSpeed);
-	if (Input::GetInstance()->GetKey(GLFW_KEY_LEFT_SHIFT))
-		transform->Translate(-up * finalSpeed);
 }
