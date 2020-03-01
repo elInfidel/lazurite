71d95a2d93f413497dc16396b5699dee974b16a6
Liam <liam@spiff.com.au>
2020-03-01 18:33:43 +1100

    Fix broken paths"

--- lazurite_core/src/Editor.cpp
+++ lazurite_core/src/Editor.cpp
@@ -2,15 +2,27 @@
 #include "subsystem/Input.h"
 #include <iostream>
 
+/*
+#include <windows.h>
+Hand little snippet for grabbing the working directory.
+std::string workingdir()
+{
+	char buf[256];
+	GetCurrentDirectoryA(256, buf);
+	return string(buf);
+}
+*/
+
 void Editor::Load()
 { 
 	auto modelPtr = testObj.AddComponent<Model>();
 	modelPtr.lock()->LoadModel("resources\\models\\PillarWoman.fbx");
-	testObj.GetComponent<Transform>().lock()->SetScale(glm::vec3(.01f, .01f, .01f));
-	testObj.GetComponent<Transform>().lock()->SetPosition(glm::vec3(0, 0, 0));
+	testObj.GetComponent<Transform>().lock()->SetScale(glm::vec3(.1f));
+	testObj.GetComponent<Transform>().lock()->SetPosition(glm::vec3(0, -15, 0));
 	testObj.GetComponent<Transform>().lock()->SetRotation((glm::radians(glm::vec3(90, 180, 180))));
 
 	auto cameraPtr = camera.AddComponent<Camera>();
+	camera.GetComponent<Transform>().lock()->SetPosition(glm::vec3(0,0,10));
 }
 
 void Editor::Tick(float deltaTime)
